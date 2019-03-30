cdda0aecff9279cb5e61fcc7eb6dbe47308b8639
Hussain <hussainmustafa31@hotmail.co.uk>
2019-03-30 19:01:34 +0500

    Length Issues #1
    Length will be doubled as user enter any number 
    
    Test Notes 
    tested when the input is 0 
    tested when the input is 1 and so on

--- PassiFy/Form1.cs
+++ PassiFy/Form1.cs
@@ -38,6 +38,8 @@ namespace PassiFy
         {
             string characters = "0123456789!@#$%^&*()_+-=ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~`;:'[]{}|,.<>/?"; //All possible characters for a password
             StringBuilder result = new StringBuilder(length); //Calls new StringBuilder
+            length = length == 1 ? length : length * 2;
+            
             for (int i=0; i < length; i++)
             {
                 result.Append(characters[random.Next(characters.Length)]); //Grab and append a new character for as long as the string goes. Pick randomly
@@ -49,11 +51,10 @@ namespace PassiFy
         {
             Random rnd = new Random();
             int lenG = Int32.Parse(textBox1.Text); //Workaround to take the text of the textbox to an int. I tried lots of workarounds before I finally got was was wrong and just parsed it.
-            string[] passWord = new string[lenG]; //Set length
-            for (int i = 0; i < passWord.Length; i++)
-            {
-                passWord[i] = GeneratorOfButt(lenG, rnd); //Call the generation function and get a random string of x length
-            }
+            string passWord = string.Empty //Set length
+            
+            passWord = GeneratorOfButt(lenG, rnd); //Call the generation function and get a random string of x length
+            
             richTextBox1.Text = String.Join("", passWord); //spit out the result in the text box. Took me time to realize I had to just join it.
         }
     }
